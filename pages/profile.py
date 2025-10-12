0a668570037ed7678f6c45421f7bf001339f7ba6
CrisantoM <cmanzano2@student.gsu.edu>
2025-10-12 18:36:35 -0400

    Front end for user profile page
    Added profile page layout, search bar for adding friends, scrollable friends list panel, and display for user profile and username

--- pages/profile.py
+++ pages/profile.py
@@ -1,11 +1,47 @@
 import dash
-from dash import html
+from dash import html, dcc, Input, Output, State
 
 dash.register_page(__name__, path='/profile')
 
 layout = html.Div([
     html.Div([
-        html.H1("This is the profile view", className="main-title"),
+        html.H1("My Profile", className="main-title"),
 
-    ], className="app-container")
+        html.Div([
+            # LEFT SIDE – profile info and Add Friend section
+            html.Div([
+                html.Div([
+                    html.Img(
+                        id='profile-image',
+
+                        # TODO: dynamically load profile image
+                        src= '',
+                        className='profile-user-image'
+                    ),
+
+                    # TODO: dynamically load username
+                    html.Span("<Username>", id="profile-username", className="profile-username"),
+                ], className="profile-info"),
+
+                html.Div([
+                    html.H2("Add Friend", className="add-friend-title"),
+                    dcc.Input(
+                        id="friend-search-input",
+                        type="text",
+                        placeholder="Search by username...",
+                        className="friend-search-input"
+                    ),
+                    html.Button("Add Friend", id="add-friend-btn", className="add-friend-btn")
+                ], className="add-friend-section")
+            ], className="profile-left"),
+
+            # RIGHT SIDE – scrollable friends list
+            html.Div([
+                html.H2("My Friends", className="friends-title"),
+                html.Ul(id="friends-list", className="friends-list")
+            ], className="profile-right")
+
+        ], className="profile-header")
+
+    ], className="app-container", id="profile-container")
 ])
