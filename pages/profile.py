3f962e0e9ff20b0f9148036cf3cf812374ffc1e5
CrisantoM <cmanzano2@student.gsu.edu>
2025-10-14 12:25:57 -0400

    removed add friend in profile
    removed the add friend input bar in the profile page, we will use the search bar in the nav section instead.

--- pages/profile.py
+++ pages/profile.py
@@ -8,34 +8,20 @@ layout = html.Div([
         html.H1("My Profile", className="main-title"),
 
         html.Div([
-            # LEFT SIDE – profile info and Add Friend section
+            # LEFT SIDE – Profile picture and username
             html.Div([
-                html.Div([
-                    html.Img(
-                        id='profile-image',
+                html.Img(
+                    id='profile-image',
+                    # TODO: dynamically load profile image
+                    src='',
+                    className='profile-user-image'
+                ),
 
-                        # TODO: dynamically load profile image
-                        src= '',
-                        className='profile-user-image'
-                    ),
+                # TODO: dynamically load username
+                html.Span("<>", id="profile-username", className="profile-username"),
+            ], className="profile-info"),
 
-                    # TODO: dynamically load username
-                    html.Span("<Username>", id="profile-username", className="profile-username"),
-                ], className="profile-info"),
-
-                html.Div([
-                    html.H2("Add Friend", className="add-friend-title"),
-                    dcc.Input(
-                        id="friend-search-input",
-                        type="text",
-                        placeholder="Search by username...",
-                        className="friend-search-input"
-                    ),
-                    html.Button("Add Friend", id="add-friend-btn", className="add-friend-btn")
-                ], className="add-friend-section")
-            ], className="profile-left"),
-
-            # RIGHT SIDE – scrollable friends list
+            # RIGHT SIDE – Scrollable friends list
             html.Div([
                 html.H2("My Friends", className="friends-title"),
                 html.Ul(id="friends-list", className="friends-list")
