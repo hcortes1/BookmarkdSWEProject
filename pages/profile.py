4c482c9fa9f4dfce04e7c182367172ae8c7664d7
hcortes1 <hcortes1@student.gsu.edu>
2025-10-15 11:15:37 -0400

    Dynamically load username and user profile using current session data.

--- pages/profile.py
+++ pages/profile.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, Input, Output, State
+from dash import html, dcc, Input, Output, State, callback
 
 dash.register_page(__name__, path='/profile')
 
@@ -12,13 +12,12 @@ layout = html.Div([
             html.Div([
                 html.Img(
                     id='profile-image',
-                    # TODO: dynamically load profile image
-                    src='',
+                    src='',  # Will be dynamically loaded from session
                     className='profile-user-image'
                 ),
 
-                # TODO: dynamically load username
-                html.Span("<>", id="profile-username", className="profile-username"),
+                # Username will be dynamically loaded from session
+                html.Span("Loading...", id="profile-username", className="profile-username"),
             ], className="profile-info"),
 
             # RIGHT SIDE – Scrollable friends list
@@ -31,3 +30,45 @@ layout = html.Div([
 
     ], className="app-container", id="profile-container")
 ])
+
+
+# Callback to dynamically load username from session
+@callback(
+    Output("profile-username", "children"),
+    Input("user-session", "data"),
+    prevent_initial_call=False
+)
+def update_profile_username(session_data):
+    """
+    Update the profile username display based on session data
+    """
+    # Check if user is logged in and session data exists
+    if not session_data:
+        return "Guest"
+    
+    if not session_data.get('logged_in', False):
+        return "Guest"
+    
+    # Get username directly from session data
+    username = session_data.get('username', 'Unknown User')
+    
+    return username
+
+
+# Callback to dynamically load profile image from session
+@callback(
+    Output("profile-image", "src"),
+    Input("user-session", "data"),
+    prevent_initial_call=False
+)
+def update_profile_image(session_data):
+    """
+    Update the profile image from session data
+    """
+    if not session_data or not session_data.get('logged_in', False):
+        return ''  # Default/placeholder image
+    
+    # Get profile image URL from session
+    profile_image_url = session_data.get('profile_image_url', '')
+    
+    return profile_image_url if profile_image_url else ''
